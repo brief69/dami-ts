b96e95172c2822240bb9f5056773259c11d33ce2
IR <ir@Imacbook.local>
2024-01-21 18:15:07 +0900

    Add a comment as to what the code is doing.

--- auth_micro/app.js
+++ auth_micro/app.js
@@ -1,41 +1,54 @@
+// 必要なモジュールをインポート
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+// ルーターモジュールをインポート
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+// Expressアプリケーションのインスタンスを作成
 var app = express();
 
-// view engine setup
+// ビューエンジンの設定
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+// ミドルウェアの設定
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// ルートハンドラの設定
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
+// 404エラーを処理するミドルウェア
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+// エラーハンドラミドルウェア
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+  // 開発環境のみエラー情報を提供
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  // エラーページをレンダリング
   res.status(err.status || 500);
   res.render('error');
 });
 
+// アプリケーションをエクスポート
 module.exports = app;
+
+// セキュリティ強化のためのミドルウェアを設定
+const helmet = require('helmet');
+const cors = require('cors');
+
+app.use(helmet());
+app.use(cors());
--- auth_micro/routes/users.js
+++ auth_micro/routes/users.js
@@ -1,9 +1,44 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+// ユーザー一覧を取得するエンドポイント
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+// ユーザー登録機能
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken'); // JWTを使用するためのモジュール
+
+router.post('/register', async (req, res) => {
+  try {
+    // パスワードをハッシュ化
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    // ユーザーをデータベースに保存するロジックをここに実装
+    res.status(201).send('User created');
+  } catch {
+    // エラー発生時は500 Internal Server Errorを返す
+    res.status(500).send();
+  }
+});
+
+// ログイン機能
+router.post('/login', (req, res) => {
+  // ユーザー検証ロジックを実装
+  // ユーザーが検証された後、JWTトークンを生成
+  const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET);
+  // トークンをレスポンスとして返す
+  res.json({ token });
+});
+
+// ユーザー情報の更新機能
+router.put('/:id', (req, res) => {
+  // ユーザー情報の更新ロジックを実装
+});
+
+// ユーザー削除機能
+router.delete('/:id', (req, res) => {
+  // ユーザーの削除ロジックを実装
 });
 
 module.exports = router;
