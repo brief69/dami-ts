a565f65bc5975608eaa69b6670fd7b7cc9a2a847
brief69 <i.r13@icloud.com>
2025-01-22 10:52:28 +0900

    docs: 型定義の問題と対処方法を開発記録に追加

--- src/infra/p2p/libp2p.ts
+++ src/infra/p2p/libp2p.ts
@@ -1,14 +1,19 @@
 import { createLibp2p } from 'libp2p'
 import { kadDHT } from '@libp2p/kad-dht'
-import { pubsub } from '@libp2p/pubsub'
+import { gossipsub } from '@chainsafe/libp2p-gossipsub'
 import { tcp } from '@libp2p/tcp'
 import { webSockets } from '@libp2p/websockets'
 import { bootstrap } from '@libp2p/bootstrap'
 import { noise } from '@chainsafe/libp2p-noise'
 import { mplex } from '@libp2p/mplex'
+import type { Libp2p } from '@libp2p/interface'
+
+interface PubSubMessage {
+  data: Uint8Array
+}
 
 export class P2PManager {
-  private node: any
+  private node: Libp2p | undefined
 
   async init() {
     this.node = await createLibp2p({
@@ -18,8 +23,14 @@ export class P2PManager {
       transports: [tcp(), webSockets()],
       connectionEncryption: [noise()],
       streamMuxers: [mplex()],
-      dht: kadDHT(),
-      pubsub: pubsub(),
+      services: {
+        pubsub: gossipsub({
+          allowPublishToZeroPeers: true
+        }),
+        dht: kadDHT({
+          clientMode: true
+        })
+      },
       peerDiscovery: [
         bootstrap({
           list: [
@@ -34,23 +45,30 @@ export class P2PManager {
     console.log('P2P node has started')
   }
 
-  async publish(topic: string, data: any) {
-    await this.node.pubsub.publish(topic, new TextEncoder().encode(JSON.stringify(data)))
+  async publish(topic: string, data: unknown) {
+    if (!this.node) throw new Error('P2P node not initialized')
+    const pubsub = this.node.services.pubsub as any
+    await pubsub.publish(topic, new TextEncoder().encode(JSON.stringify(data)))
   }
 
-  async subscribe(topic: string, handler: (data: any) => void) {
-    await this.node.pubsub.subscribe(topic, (message: any) => {
+  async subscribe(topic: string, handler: (data: unknown) => void) {
+    if (!this.node) throw new Error('P2P node not initialized')
+    const pubsub = this.node.services.pubsub as any
+    await pubsub.subscribe(topic, (message: PubSubMessage) => {
       const data = JSON.parse(new TextDecoder().decode(message.data))
       handler(data)
     })
   }
 
   async findPeers(topic: string) {
-    const peers = await this.node.pubsub.getSubscribers(topic)
+    if (!this.node) throw new Error('P2P node not initialized')
+    const pubsub = this.node.services.pubsub as any
+    const peers = await pubsub.getSubscribers(topic)
     return peers
   }
 
   async stop() {
+    if (!this.node) throw new Error('P2P node not initialized')
     await this.node.stop()
     console.log('P2P node has stopped')
   }
